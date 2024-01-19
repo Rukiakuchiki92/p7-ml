d84fc742969a3d866f83ad3c76a24fad63ebd278
Rukya92 <amelpro92@gmail.com>
2024-01-19 13:12:15 +0100

    mise a jour framework

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
+import pandas as pd
+import json
+from pydantic import BaseModel
+import pickle as p
 
+#s
 app = FastAPI()
+df = pd.read_csv("app_train_sample_clean.csv")
+df.to_pickle("hello_world.pkl")
+
+class item(BaseModel):
+    ID_CLIENT: int
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def test_predict(df):
+    with open('hello_world.pkl', 'rb') as lib:
+        corn = p.load(lib)
+    return predict
+
+@app.post("/items")
+def scoring_endpoint(item:item):
+    df = pd.DataFrame([item.model_dump().values()], columns=item.model_dump().keys())
+    lo = test_predict(df)
+    return {"predicttion":int(lo)}    
+
+
+#def read_item(item_id: int, q: str = None):
+
+
+
+    # file = open("hellow_horld.json")
+    # return json.load(file)
 
